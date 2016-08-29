525b848a1118f9f96b2212f943e5c73e44ae5d18
taivic <vicky.yen.tai@gmail.com>
2016-08-29 09:16:33 -0500

    updated js

--- app.js
+++ app.js
@@ -1,37 +1,29 @@
-$(document).ready(function() {
+
 //prompt input
-	prompt("Please enter a number");
+prompt("Please enter a number");
 
 //enter number, strings to integer
-	function fizzbuzz() {
-		var i = parseInt(prompt("Please enter a number"));
-		for (var i = 1; i <100; i++){
-			if (i % 15 === 0) {
-				console.log("fizzbuzz")
-			}
-			if (i % 3 === 0) {
-				console.log("fizz")
-			}
-			if (i % 5 === 0) {
-				console.log("buzz")
-			}
-			else {
-				console.log(i)
-			}
+function fizzbuzz() {
+	var max = parseInt(prompt("Please enter a number"))
+	return max;
+	for (var i = max; i <200; i++){
+		if (i % 15 === 0) {
+			console.log("fizzbuzz")
+		}
+		if (i % 3 === 0) {
+			console.log("fizz")
+		}
+		if (i % 5 === 0) {
+			console.log("buzz")
+		}
+		else {
+			console.log(i)
 		}
 	}
+}
 
-$(".results").append(
-	
-	)
+
 //append results
+fizzbuzz()
 
-
-});
-
-
-
-
-//var max = parseInt(prompt('Enter a number:'));
-//function startCounting {
-	//	math.floor()rid decimal;
+//math.floor()rid decimal;
